95e3ddb500a21e5a71ade534659f3fe00577848a
William <william.liu2568@gmail.com>
2023-06-24 17:43:03 -0400

    added method to decode raw transaction data from eth_sendTawTransaction request

--- src/index.js
+++ src/index.js
@@ -2,25 +2,62 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const request = require("request");
-const toBuffer = require("ethereumjs-util").toBuffer;
-const { FeeMarketEIP1559Transaction } = require("@ethereumjs/tx");
-const { open } = require("sqlite");
-const sqlite3 = require("sqlite3").verbose();
-const { default: axios } = require("axios");
-const WebSocket = require("ws");
-const path = require("path");
-const { Server } = require("ws");
-const expressWs = require("express-ws");
+const { ethers, parseTransaction } = require('ethers');
 
-const port = 3000;
+const port = 3000;  
 const test_net = "Sepolia";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-async function pass_request(req, res) {
+function decodeRawTransactionData(raw_transaction) {
+  const parsed_transaction = ethers.utils.parseTransaction(raw_transaction)
 
+  const parsed_transaction_obj = {
+    "type": parsed_transaction.type,
+    "chain_id": parsed_transaction.chainId,
+    "hash": parsed_transaction.hash,
+    "nonce": parsed_transaction.nonce,
+    "gas_limit": parsed_transaction.gasLimit.toNumber(),
+    "max_fee_per_gas": parsed_transaction.maxFeePerGas.toNumber(),
+    "max_priority_fee_per_gas": parsed_transaction.maxPriorityFeePerGas.toNumber(),
+    "from": parsed_transaction.from,
+    "to": parsed_transaction.to,
+    "data": parsed_transaction.data,
+    "value": parsed_transaction.value.toString(),
+    "v": parsed_transaction.v,
+    "r": parsed_transaction.r,
+    "s": parsed_transaction.s,
+  }
+
+  console.log("Transaction", parsed_transaction)
+
+  return parsed_transaction_obj
+}
+
+async function simulateTransaction(request_data) {
+  let raw_transaction = request_data["params"][0]
+
+  let parsed_transaction = decodeRawTransactionData(raw_transaction)
+}
+
+async function passRequest(request_data, res) {
+  request(
+    {
+      url: "environment_variable_url",
+      method: "POST",
+      json: request_data
+    },
+    (error, response, body) => {
+      if (error) {
+        console.error("Pass Request Error:", error)
+      } else {
+        console.log("Pass Request Response:", response)
+        console.log("Pass Request Body:", body)
+      }
+    }
+  )
 }
 
 app.post("/", async function (req, res) {
@@ -31,8 +68,10 @@ app.post("/", async function (req, res) {
   const request_method = request_data.method
   if (request_method == "eth_sendRawTransaction") {
     console.log("MyMiddleMan processing eth_sendRawTransaction")
+    simulateTransaction(request_data)
   } else {
     console.log(`MyMiddleMan passing request to ${test_net}`)
+    // pass_request(request_data, res)
   }
 });   
 
